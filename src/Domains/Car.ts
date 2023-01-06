259cdf0bb301a5e1475adab5253f11f2cf8e9582
João Gabriel Lyra <jgabriellyra@hotmail.com>
2023-01-06 14:20:50 -0300

    requisito 1 concluído

---
+++ src/Domains/Car.ts
@@ -0,0 +1,23 @@
+import ICar from '../Interfaces/ICar';
+
+export default class Car {
+  protected id?: string;
+  protected model: string;
+  protected year: number;
+  protected color: string;
+  protected status?: boolean;
+  protected buyValue: number;
+  private doorsQty: number;
+  private seatsQty: number;
+
+  constructor({ id, model, year, color, status, buyValue, doorsQty, seatsQty }: ICar) {
+    this.id = id;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.status = status;
+    this.buyValue = buyValue;
+    this.doorsQty = doorsQty;
+    this.seatsQty = seatsQty;
+  }
+}
