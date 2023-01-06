2e86a3b74df2b5f4c2fb7422c3f9804edae60fb8
João Gabriel Lyra <jgabriellyra@hotmail.com>
2023-01-06 20:51:43 -0300

    camada service 94% coberta

---
+++ tests/unit/Services/createCar.test.ts
@@ -0,0 +1,43 @@
+// tests/unit/services/transfer.test.ts
+
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+import { it } from 'mocha';
+import ICar from '../../../src/Interfaces/ICar';
+import CarService from '../../../src/Services/CarService';
+import Car from '../../../src/Domains/Car';
+
+describe('Deveria criar um carro', function () {
+  it('Deveria criar um carro com sucesso', async function () {
+  // Arrange
+    const carInput: ICar = {
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.990,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+
+    const carObject = {
+      id: '63b89753eb87648750ba3287',
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.99,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+
+    const carOutput = new Car(carObject);
+    sinon.stub(Model, 'create').resolves(carOutput);
+
+    const service = new CarService();
+    const result = await service.create(carInput);
+
+    expect(result).to.be.deep.equal(carOutput);
+  });
+});
---
+++ tests/unit/Services/getCars.test.ts
@@ -0,0 +1,47 @@
+// tests/unit/services/getAllTransfers.test.ts
+
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+import CarService from '../../../src/Services/CarService';
+import Car from '../../../src/Domains/Car';
+
+const inputArray = [
+  {
+    id: '63b8adc0742c31487ab937bf',
+    model: 'Marea',
+    year: 2002,
+    color: 'Black',
+    status: true,
+    buyValue: 15.99,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+  {
+    id: '63b8adfa742c31487ab937c1',
+    model: 'Fiat',
+    year: 2008,
+    color: 'White',
+    status: true,
+    buyValue: 20,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+];
+
+describe('Deveria buscar todos os carros', function () {
+  it('Deveria buscar todos os carros com sucesso', async function () {
+    // Arrange
+    const carOutput = inputArray.map((item) => new Car(item));
+    
+    sinon.stub(Model, 'find').resolves(carOutput);
+    // Act
+    const service = new CarService();
+    const result = await service.getAll();
+
+    // Assert
+    expect(result).to.be.deep.equal(carOutput);
+
+    sinon.restore();
+  });
+});
